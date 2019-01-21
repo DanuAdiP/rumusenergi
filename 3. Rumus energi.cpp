0a9c5961b9ca302f070487e55fce78d682b4e67c
DanuAdiP <danu261216@gmail.com>
2019-01-22 00:48:08 +0700

    commit pertama

---
+++ 3. Rumus energi.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+float m,c,hasil;
+cout<<"MENGHITUNG ENERGI"<<endl<<endl;
+	cout<<"Masukkan Massa            = ";cin>>m;
+	cout<<"Masukkan Kecepatan cahaya = ";cin>>c;
+	hasil=m*c*c;
+	cout<<"Maka Energinya (E=m*c*c)  = "<<hasil; 
+}
